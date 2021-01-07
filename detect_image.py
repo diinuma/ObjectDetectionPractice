6ebefc1c121e062be7aafbfd804e9b4e15114cc9
iinuma <iinumawinc@gmail.com>
2021-01-07 15:01:27 +0900

    バウンディングボックスを追加

--- detect_image.py
+++ detect_image.py
@@ -1,10 +1,10 @@
 from tflite_runtime.interpreter import Interpreter
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 import numpy as np
 import re
 
-def load_labes(path):
+def load_labels(path):
     with open(path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         labels = {}
@@ -33,6 +33,7 @@ def detect_object(interpreter, image, threshold):
     set_input_tensor(interpreter, image)
     interpreter.invoke()
 
+    boxes = get_output_tensor(interpreter, 0)
     classes = get_output_tensor(interpreter, 1)
     scores = get_output_tensor(interpreter, 2)
     count = int(get_output_tensor(interpreter, 3))
@@ -41,29 +42,49 @@ def detect_object(interpreter, image, threshold):
     for i in range(count):
         if scores[i] >= threshold:
             result = {
+                'bounding_box': boxes[i],
                 'class_id': classes[i],
                 'score': scores[i]
             }
             results.append(result)
     return results
 
+
+def annotate_objects(image, results, labels):
+    draw = ImageDraw.Draw(image)
+    size = image.size
+
+    for result in results:
+        ymin, xmin, ymax, xmax = result['bounding_box']
+        
+        xmin = int(xmin * size[0])
+        ymin = int(ymin * size[1])
+        xmax = int(xmax * size[0])
+        ymax = int(ymax * size[1])
+
+        draw.rectangle([xmin, ymin, xmax, ymax])
+        draw.text([xmin, ymin], f"{result['score']}")
+
+    image.save('with_box.jpg')
+    
+
 def main():
-    labels = load_labes('/tmp/coco_labels.txt')
+    labels = load_labels('/tmp/coco_labels.txt')
 
     interpreter = Interpreter('/tmp/detect.tflite')
     interpreter.allocate_tensors()
 
     _, input_width, input_height, _ = interpreter.get_input_details()[0]['shape']
 
-    print(interpreter.get_input_details())
-    print(interpreter.get_output_details())
+    with Image.open('sample.jpg').convert('RGB') as image:
+        resized_image = image.resize((input_width, input_height), Image.ANTIALIAS)
 
-    img = Image.open('sample.jpg').convert('RGB').resize((input_width, input_height), Image.ANTIALIAS)
+        results = detect_object(interpreter, resized_image, 0.4)
 
-    results = detect_object(interpreter, img, 0.4)
+        annotate_objects(image, results, labels)
 
-    for obj in results:
-        print(f"{labels[obj['class_id']]}: {obj['score']}")
+        for obj in results:
+            print(f"{labels[obj['class_id']]}: {obj['score']}")
 
 if __name__ == '__main__':
     main()
