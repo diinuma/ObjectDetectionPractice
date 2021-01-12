331719d006fb0731599dbf6f590b45d5c779482c
iinuma <iinumawinc@gmail.com>
2021-01-12 15:36:06 +0900

    コンソールの出力を少し変更

--- detect_camera.py
+++ detect_camera.py
@@ -8,6 +8,8 @@ from PIL import Image
 
 from detect_image import detect_object, load_labels, annotate_objects
 
+DIR = 'annotated_images'
+
 def main():
     
     labels = load_labels('/tmp/coco_labels.txt')
@@ -15,28 +17,32 @@ def main():
     interpreter = Interpreter('/tmp/detect.tflite')
     interpreter.allocate_tensors()
 
-    camera = PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
+    with PiCamera() as camera:
 
-    sleep(5)
+        camera.resolution = (1024, 768)
+        camera.start_preview()
 
-    stream = BytesIO()
-    camera.capture(stream, format='jpeg')
+        for i in range(5):
+            sleep(5)
+            
+            stream = BytesIO()
+            camera.capture(stream, format='jpeg')
 
-    stream.seek(0)
+            stream.seek(0)
 
-    with Image.open(stream) as image:
-        _, input_width, input_height, _ = interpreter.get_input_details()[0]['shape']
-        resized_image = image.convert('RGB').resize((input_width, input_height), Image.ANTIALIAS)
+            with Image.open(stream) as image:
+                _, input_width, input_height, _ = interpreter.get_input_details()[0]['shape']
+                resized_image = image.convert('RGB').resize((input_width, input_height), Image.ANTIALIAS)
 
-        results = detect_object(interpreter, resized_image, 0.4)
+                results = detect_object(interpreter, resized_image, 0.4)
 
-        for result in results:
-            print(f"{labels[result['class_id']]} : {result['score']}")
+                print(f"{i}  --------")
+                for result in results:
+                    print(f"{labels[result['class_id']]} : {result['score']}")
+                    
+                print(f"--------")
 
-        annotate_objects(image, results, labels, 'byCamera.jpg')
-
+                annotate_objects(image, results, labels, f'{DIR}/byCamera{i}.jpg')
 
 if __name__ == '__main__':
     main()
